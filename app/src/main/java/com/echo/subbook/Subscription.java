5e8f181e1717bce450c57a7f3b14f4c15066ec95
Henry Lam <nkb012pomm@gmail.com>
2018-01-29 16:56:37 -0700

    addition of class subscription

---
+++ app/src/main/java/com/echo/subbook/Subscription.java
@@ -0,0 +1,19 @@
+package com.echo.subbook;
+
+/**
+ * Created by Henry on 2018-01-29.
+ */
+
+import java.util.Date;
+
+public class Subscription {
+    private String name;
+    private Date date;
+    private int charge;
+    private String comment;
+
+    public Subscription () {
+        // nothing
+    }
+
+}
