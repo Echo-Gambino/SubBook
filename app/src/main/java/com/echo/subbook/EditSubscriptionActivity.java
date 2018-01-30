d6f5688ba80b6646190dbaff15ccf49918f6b415
Henry Lam <nkb012pomm@gmail.com>
2018-01-30 14:10:29 -0700

    Added another page for the app to possibly switch to from Add Subscription button, but it isn't fully operational yet...

---
+++ app/src/main/java/com/echo/subbook/EditSubscriptionActivity.java
@@ -0,0 +1,11 @@
+package com.echo.subbook;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Henry on 2018-01-30.
+ */
+
+public class EditSubscriptionActivity extends AppCompatActivity {
+
+}
