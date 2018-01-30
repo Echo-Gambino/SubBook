62a3e2367ed0c70df64ec09e9758f91157c3b15c
Henry Lam <nkb012pomm@gmail.com>
2018-01-29 17:14:03 -0700

    Subscription class is more fleshed out now

--- app/src/main/java/com/echo/subbook/Subscription.java
+++ app/src/main/java/com/echo/subbook/Subscription.java
@@ -12,8 +12,67 @@ public class Subscription {
     private int charge;
     private String comment;
 
-    public Subscription () {
-        // nothing
+    public Subscription() {
+        this.name = "";
+        this.date = new Date();
+        this.charge = 0;
+        this.comment = "";
+    }
+
+    public Subscription(String name) {
+        this();
+        this.name = name;
+    }
+
+    public Subscription(String name, Date date) {
+        this(name);
+        this.date = date;
+    }
+
+    public Subscription(String name, Date date, int charge) {
+        this(name, date);
+        this.charge = charge;
+    }
+
+    public Subscription(String name, Date date, int charge, String comment) {
+        this(name, date, charge);
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getCharge() {
+        return this.charge;
+    }
+
+    public void setCharge(int charge) {
+        this.charge = charge;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
     }
 
 }
+
+
+
+
