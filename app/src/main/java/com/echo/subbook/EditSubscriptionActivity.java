d2296a6f2a3e983f602f14f685365a3b8358047d
Henry Lam <nkb012pomm@gmail.com>
2018-01-31 13:06:53 -0700

    I have made a good chunk of progress in between the two Activities and learned alot of how intent and data is passed between Activities with Stackoverflow, though I could just have kept using start new activity, I felt that it would quickly eat up alot of RAM from the Activity stack getting layered one ontop of one another so frequently.

--- app/src/main/java/com/echo/subbook/EditSubscriptionActivity.java
+++ app/src/main/java/com/echo/subbook/EditSubscriptionActivity.java
@@ -1,8 +1,10 @@
 package com.echo.subbook;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -10,6 +12,7 @@ import android.widget.TextView;
  */
 
 public class EditSubscriptionActivity extends AppCompatActivity {
+    public static final String TEST_STRING = "com.echo.subbook.TEST_STRING";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,21 @@ public class EditSubscriptionActivity extends AppCompatActivity {
         TextView ESA_title = findViewById(R.id.textView_esa_title);
         ESA_title.setText(title);
 
+    }
+
+    public void AbortSubscription(View view) {
+        Intent MyIntentToReturn = new Intent(this, MainActivity.class);
+
+        MyIntentToReturn.putExtra(TEST_STRING, "edit subscription returns a value!");
+
+        /* Code for setResult(...); and other snippets of code was found in the link:
+        https://stackoverflow.com/questions/15393899/how-to-close-activity-and-go-back-to-previous-activity-in-android
+        */
+        setResult(Activity.RESULT_OK, MyIntentToReturn);
+        /* finish(); the function that retracts the activity stack by one was acquired by the link:
+        https://stackoverflow.com/questions/4038479/android-go-back-to-previous-activity
+        */
+        finish();
     }
 
 }
--- app/src/main/java/com/echo/subbook/MainActivity.java
+++ app/src/main/java/com/echo/subbook/MainActivity.java
@@ -1,12 +1,24 @@
 package com.echo.subbook;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     public static final String ESA_TITLE = "com.echo.subbook.ESA_TITLE";
+
+    private ListView subscriptionList;
+
+
+    /* public static final int for startActivityForResult & knowledge of how to use startActivityForResult found in link:
+    * https://stackoverflow.com/questions/37768604/how-to-use-startactivityforresult
+    */
+    public static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +26,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /* Code for the onActivityResult(...) {} to be able to respond to the intent of the
+    * returned Activity that Main started was from the link:
+    * https://stackoverflow.com/questions/37768604/how-to-use-startactivityforresult
+    */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            TextView total_charge_text = findViewById(R.id.textView_total_charge);
+            total_charge_text.setText("REQUEST_CODE");
+//            if (requestCode == EditSubscriptionActivity.RESULT_OK ) {
+//                total_charge_text.setText("RESULT_OK");
+//            } else if (requestCode == EditSubscriptionActivity.RESULT_CANCELED) {
+//                total_charge_text.setText("RESULT_CANCELED");
+//            }
+        }
+    }
+
     public void AddSubscription(View view) {
         Intent intent = new Intent(this, EditSubscriptionActivity.class);
 
         intent.putExtra(ESA_TITLE ,"Add Subscription");
-        startActivity(intent);
+        //startActivity(intent);
+        /* Code for startActivityForResult(...); and other snippets of code was found in the link:
+        * https://stackoverflow.com/questions/15393899/how-to-close-activity-and-go-back-to-previous-activity-in-android
+        */
+        startActivityForResult(intent, REQUEST_CODE);
+
     }
 }
