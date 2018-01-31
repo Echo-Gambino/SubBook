cb026de766c448ba999e1268129a63033553ce7b
Henry Lam <nkb012pomm@gmail.com>
2018-01-30 21:47:01 -0700

    a little more editing into the files and activities

--- app/src/main/java/com/echo/subbook/EditSubscriptionActivity.java
+++ app/src/main/java/com/echo/subbook/EditSubscriptionActivity.java
@@ -3,6 +3,7 @@ package com.echo.subbook;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by Henry on 2018-01-30.
@@ -14,6 +15,12 @@ public class EditSubscriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_subscription);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra(MainActivity.ESA_TITLE);
+
+        TextView ESA_title = findViewById(R.id.textView_esa_title);
+        ESA_title.setText(title);
 
     }
 
--- app/src/main/java/com/echo/subbook/MainActivity.java
+++ app/src/main/java/com/echo/subbook/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String ESA_TITLE = "com.echo.subbook.ESA_TITLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void AddSubscription(View view) {
         Intent intent = new Intent(this, EditSubscriptionActivity.class);
+
+        intent.putExtra(ESA_TITLE ,"Add Subscription");
         startActivity(intent);
     }
 }
