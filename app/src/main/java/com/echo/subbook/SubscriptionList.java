8faf4f4cc1137d083960f7630bcb8e4cd28b5b79
Henry Lam <nkb012pomm@gmail.com>
2018-02-03 19:55:53 -0700

    changed a few variable names

--- app/src/main/java/com/echo/subbook/SubscriptionList.java
+++ app/src/main/java/com/echo/subbook/SubscriptionList.java
@@ -39,15 +39,16 @@ import java.util.Date;
  * @see Subscription
  */
 public class SubscriptionList {
-    private ArrayList<Subscription> subList;    // ArrayList to keep track of all its Subscriptions
-    private double totalCharge;                 // totalCharge stores the total charge of all Subscriptions within ArrayList
+
+    private ArrayList<Subscription> sub_list;    // ArrayList to keep track of all its Subscriptions
+    private double total_charge;                 // totalCharge stores the total charge of all Subscriptions within ArrayList
 
     /**
      * Consctructs a SubscriptionList object
      */
     public SubscriptionList() {
-        this.subList = new ArrayList<Subscription>();   // initializes subList with an ArrayList<Subscription>
-        this.totalCharge = 0.00;                        // initialize totalCharge with 0.00
+        this.sub_list = new ArrayList<Subscription>();   // initializes subList with an ArrayList<Subscription>
+        this.total_charge = 0.00;                        // initialize totalCharge with 0.00
     }
 
     /**
@@ -57,7 +58,7 @@ public class SubscriptionList {
      */
     public SubscriptionList(ArrayList<Subscription> subList) {
         this();                     // Cascades constructor to SubscriptionList()
-        this.subList = subList;     // set subList to the given ArrayList<Subscription>
+        this.sub_list = subList;     // set subList to the given ArrayList<Subscription>
         this.refreshSum();          // calculates the sum from the given arraylist stored within the object.
     }
 
@@ -67,8 +68,8 @@ public class SubscriptionList {
      * @param subscription
      */
     public void addSubscription(Subscription subscription) {
-        totalCharge = totalCharge + subscription.getCharge();   // updates the totalCharge with the new subscription
-        subList.add(subscription);                              // adds subscription onto object's ArrayList
+        total_charge = total_charge + subscription.getCharge();   // updates the totalCharge with the new subscription
+        sub_list.add(subscription);                             // adds subscription onto object's ArrayList
     }
 
     /**
@@ -78,8 +79,8 @@ public class SubscriptionList {
      * @param index
      */
     public void delSubscription(int index) {
-        totalCharge = totalCharge - subList.get(index).getCharge(); // updates the totalCharge with removal of subscription
-        subList.remove(index);                                      // removes subscription from object's ArrayList
+        total_charge = total_charge - sub_list.get(index).getCharge(); // updates the totalCharge with removal of subscription
+        sub_list.remove(index);                                      // removes subscription from object's ArrayList
     }
 
     /**
@@ -89,34 +90,34 @@ public class SubscriptionList {
      * @return Subscription
      */
     public Subscription getSubscription(int index) {
-        return this.subList.get(index);
+        return this.sub_list.get(index);
     }
 
     /**
      * Sets the subscription within the subscription list by updating the old_subscription
      * linked to SubscriptionList with data from an unrelated new_subscription.
      *
-     * @param old_subscription
-     * @param new_subscription
+     * @param old_sub
+     * @param new_sub
      */
-    public void setSubscription(Subscription old_subscription, Subscription new_subscription) {
-        String new_name = new_subscription.getName();       // get new_sub's name
-        Date new_date = new_subscription.getDate();         // get new_sub's date
-        double new_charge = new_subscription.getCharge();   // get new_sub's charge
-        String new_comment = new_subscription.getComment(); // get new_sub's comment
+    public void setSubscription(Subscription old_sub, Subscription new_sub) {
+        String new_name = new_sub.getName();       // get new_sub's name
+        Date new_date = new_sub.getDate();         // get new_sub's date
+        double new_charge = new_sub.getCharge();   // get new_sub's charge
+        String new_comment = new_sub.getComment(); // get new_sub's comment
 
         try {
-            old_subscription.setName(new_name); // Try to set new_sub's name onto old_sub
+            old_sub.setName(new_name); // Try to set new_sub's name onto old_sub
         } catch(Exception e) {}                 // Do nothing if new_sub's name exceeds char count
 
-        old_subscription.setDate(new_date);     // Sets the new_date onto old_sub.
+        old_sub.setDate(new_date);     // Sets the new_date onto old_sub.
 
         try {
-            old_subscription.setCharge(new_charge); // Try to set new_sub's charge onto old_sub
+            old_sub.setCharge(new_charge); // Try to set new_sub's charge onto old_sub
         } catch(Exception e) {}                     // Do nothing if new_sub's charge is negative
 
         try {
-            new_subscription.setComment(new_comment);   // Try to set new_sub's comment onto old_sub
+            new_sub.setComment(new_comment);   // Try to set new_sub's comment onto old_sub
         } catch(Exception e) {}                         // Do nothing if new_comment's comment exceeds char count.
     }
 
@@ -126,17 +127,17 @@ public class SubscriptionList {
      * @return subList
      */
     public ArrayList<Subscription> getSubscriptionList() {
-        return this.subList;
+        return this.sub_list;
     }
 
     /**
      * Sets the subscription list with a given subList and refreshes the sum of
      * monetary charge from the subList's given Subscriptions.
      *
-     * @param subList
+     * @param sub_list
      */
-    public void setSubscriptionList(ArrayList<Subscription> subList) {
-        this.subList = subList;     // Let SubscriptionList's ArrayList be given ArrayList argument
+    public void setSubscriptionList(ArrayList<Subscription> sub_list) {
+        this.sub_list = sub_list;     // Let SubscriptionList's ArrayList be given ArrayList argument
         this.refreshSum();          // Recalculate the sum of SubscriptionList
     }
 
@@ -146,7 +147,7 @@ public class SubscriptionList {
      * @retun totalCharge
      */
     public double getSum() {
-        return this.totalCharge;
+        return this.total_charge;
     }
 
     /**
@@ -154,10 +155,10 @@ public class SubscriptionList {
      * */
     public void refreshSum() {
         double sum = 0.00;      // Let the sum to accumulate start at 0.00
-        for(int i = 0; i < this.subList.size(); i++) {
-            sum = sum + this.subList.get(i).getCharge();    // For every subscription in arraylist, add its subscription onto the sum
+        for(int i = 0; i < this.sub_list.size(); i++) {
+            sum = sum + this.sub_list.get(i).getCharge();    // For every subscription in arraylist, add its subscription onto the sum
         }
-        this.totalCharge = sum; // Let totalCharge be sum
+        this.total_charge = sum; // Let totalCharge be sum
     }
 
 }
